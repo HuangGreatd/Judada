7f773112aa1cf418c6f453fb2f7aa59d4ecf6419
程序员皮蛋 <13393711969@163.com>
2024-05-19 14:50:44 +0800

    后端初始化

---
+++ judada-backend/src/main/java/com/judada/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.judada.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.judada.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+*
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
